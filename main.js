d55f3df9a10a13e9ac33bf6c96bfd9ba0097b26b
MICHELLE FLANAGAN - Student <G00314881@gmit.ie>
2017-10-02 11:34:34 +0100

    first app

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ http.createServer(function (request, response) {
  response.end('Welcome to Data Rep\n');
 }).listen(8081);
 
-
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+ res.send('Hello World');
+})
+var server = app.listen(8081, function () {
+ var host = server.address().address
+ var port = server.address().port
+ console.log("Example app listening at http://%s:%s", host, port)
+})
 
 
