2d39184f934081e7706d8b5308c0421b6a5bcf3f
michelle-flan2 <32059996+michelle-flan2@users.noreply.github.com>
2017-10-09 11:47:12 +0100

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/Hello', function (req, res) {
  res.send('Hello');
@@ -7,6 +8,11 @@ app.get('/Hello', function (req, res) {
 app.get('/hello/:name', function (req, res) {
 	console.log(req.params.name);
  res.send('hello' + req.params.name);
+})
+
+app.get('/test', function (req, res) {
+	console.log("file.io");
+ res.sendFile(path.join(__dirname + '/index.html'));
 })
 var server = app.listen(8081, function () {
  var host = server.address().address
