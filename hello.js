d9fd19cfbfbabe8e94b9ef677730560fd75f22b8
michelle-flan2 <32059996+michelle-flan2@users.noreply.github.com>
2017-10-09 11:05:53 +0100

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/Hello', function (req, res) {
+ res.send('Hello');
+})
+app.get('/hello/:name', function (req, res) {
+	console.log(req.params.name);
+ res.send('hello' + req.params.name);
+})
+var server = app.listen(8081, function () {
+ var host = server.address().address
+ var port = server.address().port
+ console.log("Example app listening at http://%s:%s", host, port)
+})
